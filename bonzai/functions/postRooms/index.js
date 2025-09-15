6c13206c85978656f7db05376da90f4a2bdc0fda
Lilla Mry <maria.kjellholm@gmail.com>
2025-09-15 10:48:39 +0200

    started om postrooms

--- bonzai/functions/postRooms/index.js
+++ bonzai/functions/postRooms/index.js
@@ -1,6 +1,28 @@
+import { BatchWriteItemCommand } from "@aws-sdk/client-dynamodb";
 import { sendResponse } from "../responses";
 import { client } from "../../services/db";
 
 export const handler = async (event) => {
- return sendResponse(200, {msg: "Success!!!"})
+try {
+    const body = JSON.parse(event.body ?? "[]")
+    if (!Array.isArray(body)) return sendResponse(400, {msg: "Bodyn måste vara en array"})
+
+    const putRequest = body.map((room) => {
+        return {
+            PutRequest: {
+                Item: {
+                    pk: {S: "ROOMS"},
+                    sk: {S: room.roomNo.toString()},
+                    roomNo: {N: room.roomNo},
+                    
+                }
+            }
+        }
+    })
+
+    return sendResponse(200, {msg: "Success!!!"})
+} catch (error) {
+    
+}
+
 };
