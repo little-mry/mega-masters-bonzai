2762211164f2dc1da1d9124465bf032251e7c714
Lucas Söderlund <superlucke123@gmail.com>
2025-09-15 12:18:01 +0200

    changed to QueryCommand instead of ScanCommand

--- bonzai/functions/getRooms/index.js
+++ bonzai/functions/getRooms/index.js
@@ -1,14 +1,14 @@
-import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { QueryCommand } from "@aws-sdk/client-dynamodb";
 import { sendResponse } from "../responses/index.js";
-import { serverError } from "../responses/errors.js";
-import { client } from "../../services/db"; 
+import { serverError } from "../responses/error.js";
+import { client } from "../../services/db.js";
 
 export const handler = async (event) => {
   try {
     const result = await client.send(
-      new ScanCommand({
+      new QueryCommand({
         TableName: "bonzai-table",
-        FilterExpression: "pk = :pk",
+        KeyConditionExpression: "pk = :pk",
         ExpressionAttributeValues: {
           ":pk": { S: "ROOM" },
         },
@@ -16,18 +16,19 @@ export const handler = async (event) => {
     );
 
     const rooms = result.Items.map((item) => ({
-        roomNo: Number(item.roomNo.N),
-        roomName: item.roomName.S,
-        roomType: item.roomType.S,
-        guestsAllowed: Number(item.guestsAllowed.N),
-        price: Number(item.price.N),
-        isAvailable: item.isAvailable.BOOL,
-        createdAt: item.createdAt.S,
-        modifiedAt: item.modifiedAt.S,
+      roomNo: Number(item.roomNo.N),
+      roomName: item.roomName.S,
+      roomType: item.roomType.S,
+      guestsAllowed: Number(item.guestsAllowed.N),
+      price: Number(item.price.N),
+      isAvailable: item.isAvailable.BOOL,
+      createdAt: item.createdAt.S,
+      modifiedAt: item.modifiedAt.S,
     }));
+
     return sendResponse(200, { rooms });
   } catch (error) {
     console.error("Fel vid hämtning av rum:", error);
     return serverError("Fel vid hämtning av rum");
   }
-}
+};
