65f9f44c0f61a8d7167cb144399477623ad74eb9
Lilla Mry <maria.kjellholm@gmail.com>
2025-09-16 12:55:06 +0200

    done GSI1 and 2 + made querylogic in getBookings

--- bonzai/functions/getBookings/index.js
+++ bonzai/functions/getBookings/index.js
@@ -1,6 +1,48 @@
 import { sendResponse } from "../responses";
+import {
+  badRequest,
+  serverError,
+  notFound,
+  conflict,
+} from "../responses/errors";
 import { client } from "../../services/db";
+import { isIsoDate } from "./service";
 
 export const handler = async (event) => {
-return sendResponse(200, {msg: "Success!!!"})
+  try {
+    const query = event.queryStringParameters || {};
+
+    const date = query.date?.trim();
+    const from = query.from?.trim();
+    const to = query.to?.trim();
+
+    if (date && (from || to)) {
+      return badRequest("Ange antingen ?date=YYYY-MM-DD ELLER ?from=YYYY-MM-DD&to=YYYY-MM-DD");
+    }
+    if (date && !isIsoDate(date)) {
+      return badRequest("Ogiltigt date-format, använd YYYY-MM-DD");
+    }
+    if ((!from && to) || (from && !to)) {
+        return badRequest("Både from och to måste anges");
+    }
+    if (from && (!isIsoDate(from) || !isIsoDate(to))) {
+        return badRequest("Ogiltigt from/to-format, använd YYYY-MM-DD");
+    }
+    if (from && !(from < to)) {
+        return badRequest("'from' måste vara före 'to'");
+    }
+
+    let mode = "ALL"
+    if (date) {
+        mode = "DAY"
+    }
+    if (from && to) {
+        mode = "INTERVAL"
+    }
+    return sendResponse(200, {mode,  params: { date, from, to },
+      note: "Härnäst: anropa GSI1 (ALL) eller GSI2 (DAY/INTERVAL) enligt valt mode."} )
+
+  } catch (error) {
+    return serverError()
+  }
 };
